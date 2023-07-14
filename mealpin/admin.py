15d2c649fafd4ac5d4da630e461c031e211cd33d
james hart <jamesmurphy197197@gmail.com>
2023-07-14 18:26:02 +0000

    [SETUP] Add Django Summernote

--- mealpin/admin.py
+++ mealpin/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Meal
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('recipe')
+
+
+admin.site.register(Meal)
