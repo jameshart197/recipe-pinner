3642662b37465d1c03df8a258a00399c802e36c0
james hart <jamesmurphy197197@gmail.com>
2023-07-14 18:21:31 +0000

    [FEAT] Add Meal model

--- mealpin/models.py
+++ mealpin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model):
+    MEAL_TYPE = ((0, "Breakfast"), (1, "Lunch"), (2, "Dinner"), (3, "Dessert"), (4, "Snack"))
+    title = models.CharField(max_length=200, unique=True)
+    meal_type = models.IntegerField(choices=MEAL_TYPE, default=0)
+    preptime_in_minutes = models.DecimalField(max_digits=3, decimal_places=1)
+    calories_in_grams = models.DecimalField(max_digits=5, decimal_places=1)
+    protein_in_grams = models.DecimalField(max_digits=4, decimal_places=1)
+    fat_in_grams = models.DecimalField(max_digits=4, decimal_places=1)
+    carbs_in_grams = models.DecimalField(max_digits=4, decimal_places=1)
+    recipe = models.TextField(default="This recipe has not been added yet!")
+
+    def __str__(self):
+        return f'{self.title} - {self.preptime_in_minutes} minutes to prep - {self.calories_in_grams} Kcal'
