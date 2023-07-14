0641089fd34ed766ffd68de91c1af02b676c8377
james hart <jamesmurphy197197@gmail.com>
2023-07-14 18:16:53 +0000

    [FEAT] Add index.html with relevant url and view

--- mealpin/views.py
+++ mealpin/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
