755ebb8dc2b9616f824786dbab40e730a9d29cce
james hart <jamesmurphy197197@gmail.com>
2023-07-17 22:20:28 +0000

    [FEAT] Add Info Dialog functionality

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,24 +1,38 @@
-const openInfoButtons = document.getElementsByClassName("open-info-button");
-const closeInfoButtons = document.getElementsByClassName("close-info-button");
-const dialogContent = document.getElementsByTagName("article")
+let openInfoButtons = document.getElementsByClassName("open-info-button");
+const dialogContent = document.getElementsByTagName("dialog")[0];
 
-openInfoButtons.array.forEach(element => {
+function closeButton() {
+    dialogContent.close();
+}
+
+[...openInfoButtons]?.forEach(element => {
+    console.log("I am working");
     element.addEventListener("click", () => {
-        modal.showModal()
+        const mealId = element.dataset.meal;
+        const url = `info/dialog/${mealId}`;
+        dialogContent.showModal();
+        fetch(url).then((response)=> {
+            return response.text();
+        }).then((html)=>{
+            dialogContent.children[1].innerHTML = html;
+            // .children[1] targets the article within the dialogContent section
+        }).catch((err)=> {
+            console.warn('Something went wrong.', err);
+            dialogContent.innerHTML = '';
+        })
     })
 });
 
-closeInfoButtons.array.forEach(element => {
-    element.addEventListener("click", () => {
-        modal.close()
-    })
-});
+// clicking outside the dialogue box closes box
 
-fetch(url).then((response)=> {
-    return response.text();
-}).then((html)=>{
-    dialogContent.innerHTML = html;
-}).catch((err)=> {
-    console.warn('Something went wrong.', err);
-    dialogContent.innerHTML = '';
+dialogContent.addEventListener("click", e => {
+    const dialogDimensions = dialogContent.getBoundingClientRect()
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top || 
+        e.clientY > dialogDimensions.bottom
+    ) {
+        dialogContent.close();
+    }
 })
